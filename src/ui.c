e8245586133263dbb09b218b734d296d628a2afa
rvba <mil01@free.fr>
2013-05-29 18:08:58 +0200

    change intro

--- src/ui.c
+++ src/ui.c
@@ -7,6 +7,7 @@
  *
  */
 
+#include "util.h"
 #include "op.h"
 #include "context.h"
 #include "scene.h"
@@ -24,9 +25,18 @@
 #include "camera.h"
 #include "list.h"
 #include "data.h"
+#include "camera.h"
 
 t_txt *txt_intro;
 t_txt *txt_version;
+
+int frame_offset=0;
+float *stars;
+float *stars_color;
+int star_init=0;
+int star_count=1000;
+int *stars_chance;
+float *stars_velocity;
 
 void ui_draw_mouse(void)
 {
@@ -73,27 +83,193 @@ void ui_draw_lines(void)
 	}
 }
 
+
+int get_sign(void)
+{
+	int i = u_randrange(0,2);
+	if(i>=1)
+		return -1;
+	else
+		return 1;
+}
+
+int get_chance(void)
+{
+	int tot = 500;
+	int range = 400;
+	int i = u_randrange(0,tot);
+	if(i>=range)
+		return 1;
+	else
+		return 0;
+}
+
+int get_low_chance(void)
+{
+	int tot = 500;
+	int range = 450;
+	int i = u_randrange(0,tot);
+	if(i>=range)
+		return 1;
+	else
+		return 0;
+}
+
+void star_intensity(int i)
+{
+	int sign = get_sign();
+	float *c = stars_color + (i*3);
+	c[0]+=sign*.1;
+	c[1]+=sign*.1;
+	c[2]+=sign*.1;
+}
+
+
+void star_mvt(int i)
+{
+	t_context *C = ctx_get();
+	if(stars_chance[i] && i > 1)
+	{
+		float *s = stars + (i*3);
+		float *s_next = s -1; 
+
+		float v[3];
+
+		int pos = C->app->mouse->x;
+		float acc = 1;
+
+		if(i > pos)
+			acc = 4;
+		if(i == pos)
+			acc = 50;
+
+		vsub(v,s_next,s);
+		vmul(v,stars_velocity[i]*.001*acc);
+		vadd(s,s,v);
+	}
+}
+
 void ui_draw_intro(void)
 {
 	t_context *C=ctx_get();
 
 	if(C->ui->show_intro)
 	{
-		float scale=6;
-		float scv = .25;
+		int time_limit = 25;
+
+		float scale= 5;
+		float scv = .3;
 		float p[3]={(C->app->window->width)/4,(C->app->window->height)/2,0};
+		float offset = 10;
+		glPushMatrix();
+		glTranslatef(p[0]-offset,p[1]-offset,p[2]);
+
+		glPopMatrix();
+
 		glPushMatrix();
 		glTranslatef(p[0],p[1],p[2]);
+
 		glScalef(scale,scale,scale);
 		float z = C->ui->zoom;
 		C->ui->zoom = 5;
+
+		glPushMatrix();
+
+		// Minuit
 		txt_intro->draw(txt_intro);
+
+		// Version
 		C->ui->zoom = z;
-		glTranslatef(32,-1,0);
-		//glTranslatef(0,-3,0);
+		glTranslatef(35,0,0);
 		glScalef(scv,scv,scv);
 		txt_version->draw(txt_version);
+
+		// Camera
+		t_camera *camera = C->ui->camera;
+		op_camera_frustum_init(camera);
+		camera->type = camera_frustum;
+		camera->target[0]=500;
+		camera->target[1]=500;
+		camera->target[2]=500;
+
+		camera->eye[0]=0;
+		camera->eye[1]=0;
+		camera->eye[2]=0;
+
+		int dist = 500;
+		int color_factor = 100;
+
+		op_camera_switch_3d(C,C->ui->camera);
 		glPopMatrix();
+
+		//glLoadIdentity();
+
+		int frame = C->app->frame;
+		float rot = (float)frame*.05;
+
+		glRotatef(rot,0,0,1);
+		glRotatef(rot,0,1,0);
+		glRotatef(rot,1,0,0);
+
+		if(C->app->frame > time_limit)
+		{
+			int i;
+			if(!star_init)
+			{
+				star_init = 1;
+				stars = malloc(sizeof(float)* star_count * 3);
+				stars_color = malloc(sizeof(float)* star_count * 3);
+				stars_chance = malloc(sizeof(int) * star_count);
+				stars_velocity = malloc(sizeof(float) * star_count);
+				for(i=0;i<star_count;i++)
+				{
+					stars[(i*3)+0] = u_randrange(0,dist)*get_sign();
+					stars[(i*3)+1] = u_randrange(0,dist)*get_sign();
+					stars[(i*3)+2] = u_randrange(0,dist)*get_sign();
+
+					float c = (float)u_randrange(0,color_factor)/color_factor;
+					stars_color[(i*3)+0] = c;
+					stars_color[(i*3)+1] = c;
+					stars_color[(i*3)+2] = c;
+
+					if(get_chance()) stars_chance[i] = 1;
+					else stars_chance[i] = 0;
+
+					stars_velocity[i] = (float)u_randrange(0,1000)/1000;
+				}
+			}
+
+			float *s=stars;
+			float *col=stars_color;
+			float col_var[3];
+			float iii = (float) C->app->frame / 500;
+			for(i=0;i<star_count;i++)
+			{
+				vcp(col_var,col);
+				vmul(col_var,iii);
+
+				star_intensity(i);
+				skt_point(s,1,col_var);
+				star_mvt(i);
+				s+=3;
+				col+=3;
+			}
+
+		}
+
+
+		camera->type = camera_ortho;
+
+		glPopMatrix();
+
+	}
+	else
+	{
+		if(stars) free(stars);
+		if(stars_color) free(stars_color);
+		if(stars_velocity) free(stars_velocity);
+
+		star_init = 0;
 	}
 }
 
