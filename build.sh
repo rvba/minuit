1a71c67cc4f7754481f49caaaa50c7d40bc44735
rvba <mil01@free.fr>
2013-12-11 01:50:42 +0100

    build

--- build.sh
+++ build.sh
@@ -1,16 +1,28 @@
 #/bin/bash
-sudo apt-get install 	build-essential \
-			cmake\
-			libgl1-mesa-dev\
-			freeglut3-dev\
-			libjpeg62-dev\
-			libpng12-dev\
-			libxmu-dev\
-			libxi-dev\
 
-mkdir minuit-build
-cd minuit-build
-cmake ../minuit-dev
+echo "m i n u i t "
+echo "install dependencies ? "
+read -r input
+if [[ $input == "y" ]];then
+	sudo apt-get install 	build-essential \
+				cmake\
+				libgl1-mesa-dev\
+				freeglut3-dev\
+				libjpeg62-dev\
+				libpng12-dev\
+				libxmu-dev\
+				libxi-dev
+fi
+
+echo "build name:"
+read -r input
+cd ..
+mkdir $input
+cd $input
+echo "cmaking..."
+cmake ../minuit
+echo "making..."
 make
+echo "done."
 
 
