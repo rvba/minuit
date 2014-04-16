6c8d270e488d7532c92f3317d0cfd18f68795f63
rvba <mil01@free.fr>
2014-04-16 19:30:31 +0200

    refactor

--- src/lib/viewport.c
+++ src/lib/viewport.c
@@ -259,14 +259,6 @@ t_viewport *viewport_rebind( t_scene *sc, void *ptr)
 	return viewport;
 }
 
-/*
-t_node *viewport_add(const char *name)
-{
-	t_node *node=viewport_make(name);
-	return node;
-}
-*/
-
 // CLONE
 
 t_viewport *viewport_clone(t_viewport *viewport)
@@ -318,50 +310,6 @@ void viewport_add_controls( t_viewport *viewport)
 	viewport->controls = block;
 
 }
-
-// MAKE
-
-/*
-t_node *viewport_make(const char *name)
-{
-	t_context *C = ctx_get();
-
-	t_node *node_viewport = scene_add(C->scene, dt_viewport, name);
-	t_node *node_camera = op_new_camera("vp_camera");
-	t_object *object = node_camera->data;
-
-	t_viewport *viewport = node_viewport->data;
-	t_camera *camera = object->data;
-
-	viewport->camera = camera;
-	viewport->width = C->app->window->width;
-	viewport->height = C->app->window->height;
-	viewport->x = 0;
-	viewport->y = 0;
-
-	scene_add_ref(C->scene,"struct_ref","viewport","width",&viewport->width,viewport);
-	scene_add_ref(C->scene,"struct_ref","viewport","height",&viewport->height,viewport);
-	scene_add_ref(C->scene,"struct_ref","viewport","x",&viewport->x,viewport);
-	scene_add_ref(C->scene,"struct_ref","viewport","y",&viewport->y,viewport);
-	scene_add_ref(C->scene,"struct_ref","viewport","fullscreen",&viewport->fullscreen,viewport);
-
-	t_block *block = add_block_block( C, "viewport_controls");
-	if( block)
-	{
-		block->pos[0] = 0;
-		block->pos[1] = 1;
-		add_brick_slider_int( C, block, "width", &viewport->width);
-		add_brick_slider_int( C, block, "height", &viewport->height);
-		add_brick_slider_int( C, block, "x", &viewport->x);
-		add_brick_slider_int( C, block, "y", &viewport->y);
-		add_brick_switch( C, block, "fullscreen", &viewport->fullscreen);
-	}
-
-	viewport->controls = block;
-
-	return node_viewport;
-}
-*/
 
 void _viewport_free(t_viewport *viewport)
 {
