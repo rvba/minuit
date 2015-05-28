c8708314173ebb5fbb0fd00201324c98e4a160c3
rvba <mil01@free.fr>
2015-05-28 20:47:33 +0200

    draw edges: add front color

--- src/draw/draw_mesh_edge.c
+++ src/draw/draw_mesh_edge.c
@@ -9,6 +9,8 @@ void draw_mesh_edge( t_draw *draw, struct Mesh *mesh)
 {
 	if( draw->with_edge)
 	{
+		float *c = draw->front_color;
+		glColor3f(c[0],c[1],c[2]);
 		glVertexPointer(3,GL_FLOAT,0,(float *)mesh->vertex->data);
 		glDrawElements( GL_LINES, mesh->edges->count * 2, GL_UNSIGNED_INT, mesh->edges->data); 
 	}
