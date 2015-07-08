8e40e22126a182d0f3e6998949036c9ebb1ddfef
rvba <mil01@free.fr>
2015-07-08 09:15:45 +0200

    refactor

--- src/ops/op_add_object.c
+++ src/ops/op_add_object.c
@@ -100,15 +100,6 @@ t_node *op_new_cube(const char *name)
 	int *tris = NULL;
 		
 	t_node *cube = mesh_make(name,tot_vertex,tot_face,tot_quad,tot_tri,verts,quads,tris);
-	t_mesh *mesh = cube->data;
-
-	// Edges
-	//mesh->edges = vlst_make( "edges", dt_uint, 2, 12, cube_edges);
-	/*
-	mesh->edges = vlst_make( "edges", dt_uint, 2, 12, NULL);
-	_loop_unsigned_int(mesh->edges->data, cube_edges, mesh->edges->count, mesh->edges->size);
-	mesh->state.with_line =1;
-	*/
 
 	return cube;
 }
